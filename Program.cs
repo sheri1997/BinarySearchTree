87dbaa46fd6f59ba514960686b43629053fd9e99
Shreesh Pandey <shvspandey@gmail.com>
2021-11-12 17:20:57 +0530

    Displaying Welcome Message On The Master Branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Binary Search Tree Program");
+        }
+    }
+}
